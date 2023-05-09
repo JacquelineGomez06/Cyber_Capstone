06b50826be8af5b09cc130623e1b105cd6d2cf72
“jacquelinegomez06” <jacquelinegomez06@gmail.com>
2023-05-08 22:01:59 -0400

    create sql db

---
+++ sql/sql_db.py
@@ -0,0 +1,19 @@
+import psycopg2
+import psycopg2.extras
+from config import params
+import csv
+import os
+
+
+conn = psycopg2.connect(**params)
+
+with conn.cursor() as cursor:
+    # READ FILES IN PYTHON
+    with open('sql/schema.sql', 'r') as schema:
+        # reads in entire file
+        queries = schema.read()
+        print(queries)
+        # starting a transaction (or continuing)
+        cursor.execute(queries)
+    # commit changes
+    conn.commit()
