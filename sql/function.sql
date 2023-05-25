898f6090cd34a2f453044cfa712eb00451437617
“jacquelinegomez06” <jacquelinegomez06@gmail.com>
2023-05-24 20:52:43 -0400

    schema update with new tables

--- sql/function.sql
+++ sql/function.sql
@@ -8,7 +8,8 @@ CREATE OR REPLACE FUNCTION cyber.largest_intrusion() RETURNS VARCHAR AS $$
     BEGIN
         -- basic sql query to get name of company with most breaches
         SELECT Name_of_Covered_Entity, MAX(Individuals_Affected)
-            FROM cyber
+            FROM cyber.intrusions
+            GROUP BY Name_of_Covered_Entity
             LIMIT 1;
         INTO Name_of_Covered_Entity;
         RETURN Name_of_Covered_Entity;
@@ -17,4 +18,31 @@ CREATE OR REPLACE FUNCTION cyber.largest_intrusion() RETURNS VARCHAR AS $$
 -- specify language 
 LANGUAGE plpgsql;
 
+-- function 2
+CREATE OR REPLACE FUNCTION cyber.top_breach() RETURNS INT AS $$ 
+    DECLARE
+         
+    BEGIN
+        -- Return name of the breach that occurs most often
+        SELECT 
+            FROM 
+        INTO ;
+        RETURN ;
+    END; $$ 
+LANGUAGE plpgsql;
 
+-- function 3 
+CREATE OR REPLACE FUNCTION cyber.breach_medians() RETURNS VARCHAR AS $$ 
+    DECLARE
+       
+    BEGIN
+        --Return each unique breach and its respective median number of individuals affected
+        SELECT 
+        
+        INTO ;
+        
+        RETURN ;
+
+    END; $$ 
+
+LANGUAGE plpgsql;
--- sql/schema.sql
+++ sql/schema.sql
@@ -2,7 +2,9 @@ DROP SCHEMA IF EXISTS cyber CASCADE;
 CREATE SCHEMA cyber ;
 
 CREATE TABLE cyber.intrusions(
-    INTRUSION_ID INT PRIMARY KEY,
+    Intrusion_ID INT PRIMARY KEY,
+    Location_ID INT,
+    Type_ID INT,
     Name_of_Covered_Entity VARCHAR(255),
     State VARCHAR(255),
     Individuals_Affected INT,
@@ -24,6 +26,42 @@ CREATE TABLE cyber.intrusions(
     Location_of_Breached_Information_Paper INT
     
 );
+
+CREATE TABLE cyber.breaches(
+    Type_ID INT PRIMARY KEY,
+    Intrusion_ID INT,
+    Location_ID INT,
+    Type_of_Breach_Hacking_IT_Incident INT,
+    Type_of_Breach_Improper_Disposal INT,
+    Type_of_Breach_Loss INT,
+    Type_of_Breach_Other INT,
+    Type_of_Breach_Theft INT,
+    Type_of_Breach_Unauthorized_Access_Disclosure INT
+);
+
+CREATE TABLE cyber.locations(
+    Location_ID INT PRIMARY KEY,
+    Type_ID INT,
+    Intrusion_ID INT,
+    Location_of_Breached_Information_Desktop_Computer INT,
+    Location_of_Breached_Information_Laptop INT,
+    Location_of_Breached_Information_Network_Server INT,
+    Location_of_Breached_Information_Other INT,
+    Location_of_Breached_Information_Other_Portable_Electronic_Device INT,
+    Location_of_Breached_Information_Paper INT
+);
+
+ALTER TABLE cyber.intrusions
+ADD CONSTRAINT fk_breaches FOREIGN KEY (Type_ID) REFERENCES cyber.breaches(Type_ID),
+ADD CONSTRAINT fk_locations FOREIGN KEY (Location_ID) REFERENCES cyber.locations(Location_ID);
+
+ALTER TABLE cyber.breaches
+ADD CONSTRAINT fk_intrusions FOREIGN KEY (Intrusion_ID) REFERENCES cyber.intrusions(Intrusion_ID),
+ADD CONSTRAINT fk_locations FOREIGN KEY (Location_ID) REFERENCES cyber.locations(Location_ID);
+    
+ALTER TABLE cyber.locations
+ADD CONSTRAINT fk_intrusions FOREIGN KEY (Intrusion_ID) REFERENCES cyber.intrusions(Intrusion_ID),
+ADD CONSTRAINT fk_breaches FOREIGN KEY (Type_ID) REFERENCES cyber.breaches(Type_ID);
 
 GRANT ALL PRIVILEGES ON all tables in SCHEMA cyber TO postgres;
 
